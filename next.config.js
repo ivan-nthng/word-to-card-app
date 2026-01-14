e2b92b1e28af840bcc5944936241c9d00f2116a0
ivan.cheremisin <icheremisin@gmail.com>
2026-01-14 12:24:20 -0300

    Add webpack ignore-loader for prisma.config.ts to fix Vercel build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	webpack: (config) => {
+		// Ignore prisma.config.ts if it exists
+		config.module.rules.push({
+			test: /prisma\.config\.ts$/,
+			use: 'ignore-loader',
+		});
+		return config;
+	},
 };
 
 module.exports = nextConfig;
